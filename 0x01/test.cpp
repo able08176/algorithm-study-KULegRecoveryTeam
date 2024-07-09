a878a97f2d7b2f1123db09faaa1805045e2b36b5
able08176 <able08176@gmail.com>
2024-07-10 03:10:34 +0900

    jaeyoung 0x01 test complete

--- 0x01/test.cpp
+++ 0x01/test.cpp
@@ -6,19 +6,60 @@
 using namespace std;
 
 int func1_jaeyoung(int N){
-    return -1;
+    // 문제 1
+    // N 이하의 자연수 중에서 3의 배수이거나 5의 배수인 수를 모두 합한 값을 반환하는 함수 func1(int N)을 작성하라.
+    // N은 10만 이하의 자연수이다.
+
+    int sum = 0;
+    for (int i = 1; i <= N; ++i) {
+        if ((i % 3 == 0) || (i % 5 == 0))
+            sum += i;
+    }
+    return sum;
+    // 시간복잡도: O(N)
 }
 
 int func2_jaeyoung(int arr[], int N){
-    return -1;
+    // 문제 2
+    // 주어진 길이 N의 int 배열 arr에서
+    // 합이 100인 서로 다른 위치의 두 원소가 존재하면 1을,
+    // 존재하지 않으면 0을 반환하는 함수 func2(int arr[], int N)을 작성하라.
+    // arr의 각 수는 0 이상 100 이하이고 N은 1000 이하이다.
+
+    for (int i = 0; i < N - 1; ++i) {
+        for (int j = i + 1; j < N; ++j) {
+            if (arr[i] + arr[j] == 100)
+                return 1;
+        }
+    }
+    return 0;
+    // 시간복잡도: O(N^2)
 }
 
 int func3_jaeyoung(int N){
-    return -1;
+    // 문제 3
+    // N이 제곱수이면 1을 반환하고 제곱수가 아니면 0을 반환하는
+    // 함수 func3(int N)을 작성하라. N은 10억 이하의 자연수이다.
+
+    for (int i = 1; i * i <= N; ++i) {
+        if (i * i == N)
+            return 1;
+    }
+    return 0;
+    // 시간복잡도: O(sqrt(N))
 }
 
 int func4_jaeyoung(int N){
-    return -1;
+    // 문제 4
+    // N이하의 수 중에서 가장 큰 2의 거듭제곱수를 반환하는 함수 func4(int N)을 작성하라.
+    // N은 10억 이하의 자연수이다.
+
+    int temp;
+    for (int i = 1; i <= N; i*=2) {
+        temp = i;
+    }
+    return temp;
+    // 시간복잡도: O(log N)
 }
 
 int func1_yunseong(int N){
