e7ada6225c166bef38b923b2fee2b907487b08c7
lwb1112 <165344710+lwb1112@users.noreply.github.com>
2024-07-15 19:39:19 +0900

    Update 0x02workbook_WB.cpp

--- 0x02/wookbin/0x02workbook_WB.cpp
+++ 0x02/wookbin/0x02workbook_WB.cpp
@@ -1,1 +1,67 @@
+#include <stdio.h>
 
+/* 문제: 10871
+int main(void) {
+	int n, x;
+	scanf("%d %d", &n, &x);
+
+	int num;
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &num);
+		if (num < x) printf("%d ", num);
+	}
+	return 0;
+}
+*/
+
+/* 문제: 2309 
+int main(void) {
+	int height[9];
+	int sum = 0;
+	int over = 0;
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &height[i]);
+		sum = sum + height[i];
+	}
+	over = sum - 100;
+
+	int a = 0;
+	int b = 0;
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (height[i] + height[j] == over) {
+				a = i;
+				b = j;
+				break;
+			}
+		}
+	}
+
+	int tmp;
+	int dwarf[7];
+	int c = 0;
+	for (int i = 0; i < 9; i++) {
+		if (i != a && i != b) {
+			dwarf[c] = height[i];
+			c++;
+		}
+	}
+
+
+	for (int i = 0; i < 7; i++) {
+		for (int j = i + 1; j < 7; j++) {
+			if (dwarf[i] > dwarf[j]) {
+				tmp = dwarf[i];
+				dwarf[i] = dwarf[j];
+				dwarf[j] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 7; i++) {
+		printf("%d\n", dwarf[i]);
+	}
+
+	return 0;
+}
+*/
