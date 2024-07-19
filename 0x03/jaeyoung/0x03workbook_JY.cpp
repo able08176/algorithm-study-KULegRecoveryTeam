b53488362d78d432a4258e13748c11643672dcbe
able08176 <able08176@gmail.com>
2024-07-19 21:10:10 +0900

    jaeyoung 0x03workbook_JY.cpp complete!!

--- 0x03/jaeyoung/0x03workbook_JY.cpp
+++ 0x03/jaeyoung/0x03workbook_JY.cpp
@@ -2,6 +2,7 @@
 // Created by Admin on 2024-07-14.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main(void) {
@@ -53,4 +54,30 @@ int main(void) {
     // }
     // ans = max(ans, (freq[6] + freq[9] + 1) / 2);
     // cout << ans;
+
+    // BOJ 3273
+    // 문제의 조건을 확장시켜보자.
+    // BOJ 3273 문제는 n개의 서로 다른 양의 정수로 이루어진 수열을 받지만,
+    // 수열 안의 양의 정수들이 서로 중복될 수 있는 조건 하의 코드를 짜보았다.
+
+    // 배열 인덱스를 다룰 때는 항상 런아웃 에러 OutofBounds 조심!!
+    // 변수의 값 범위 항상 잘 고려하기!!
+    int n;
+    int indexes[100000] = {};
+    int x;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> indexes[i];
+    }
+    cin >> x;
+
+    short occur[2000001];
+    fill(occur, occur + 2000001, 0);
+    int freq = 0;
+    for (int i = 0; i < n; ++i) {
+        if (x > indexes[i] && occur[x - indexes[i]])
+            freq += occur[x - indexes[i]];
+        occur[indexes[i]]++;
+    }
+    cout << freq;
 }
