75cb6c01ec13afa2df8bd5eaca302c77bb2ef036
able08176 <able08176@gmail.com>
2024-07-19 21:20:50 +0900

    jaeyoung 0x03workbook_JY.cpp 메모 추가!

--- 0x03/jaeyoung/0x03workbook_JY.cpp
+++ 0x03/jaeyoung/0x03workbook_JY.cpp
@@ -62,22 +62,53 @@ int main(void) {
 
     // 배열 인덱스를 다룰 때는 항상 런아웃 에러 OutofBounds 조심!!
     // 변수의 값 범위 항상 잘 고려하기!!
-    int n;
-    int indexes[100000] = {};
-    int x;
+
+//    int n;
+//    int indexes[100000] = {};
+//    int x;
+//    cin >> n;
+//    for (int i = 0; i < n; ++i) {
+//        cin >> indexes[i];
+//    }
+//    cin >> x;
+//
+//    short occur[2000001];
+//    fill(occur, occur + 2000001, 0);
+//    int freq = 0;
+//    for (int i = 0; i < n; ++i) {
+//        if (x > indexes[i] && occur[x - indexes[i]])
+//            freq += occur[x - indexes[i]];
+//        occur[indexes[i]]++;
+//    }
+//    cout << freq;
+
+    // BOJ 3273 별해
+    // 두 수의 합을 다루는 문제에서 내가 처음에 틀렸던 코드,
+    // 먼저 occur 올리고, 그 다음에 체크하는 알고리즘의 문제는
+    // 중간값에서 문제가 발생했음.
+    // 밑의 별해는 먼저 occur을 올리고, 그 다음에 체크시 체크하는 횟수를
+    // 반만 시행하므로 중간값에서 문제 방지.
+
+    // 각 자연수의 존재 여부를 저장하는 배열, 배열의 크기를 200만으로 두어
+    // 25번째 줄에서 x-i의 범위를 별도로 체크하지 않아도 되게끔 했다.
+    int a[2000001] = {};
+
+    int n, t, x, ans = 0;
     cin >> n;
     for (int i = 0; i < n; ++i) {
-        cin >> indexes[i];
+        cin >> t;
+        a[t]++;
     }
-    cin >> x;
 
-    short occur[2000001];
-    fill(occur, occur + 2000001, 0);
-    int freq = 0;
-    for (int i = 0; i < n; ++i) {
-        if (x > indexes[i] && occur[x - indexes[i]])
-            freq += occur[x - indexes[i]];
-        occur[indexes[i]]++;
+    cin >> x;
+    for (int i = 1; i < (x + 1) / 2; ++i) {
+        // 합이 x가 되는 서로 다른 자연수 (i,x-i)가 존재할 경우
+        if (a[i] == 1 && a[x - i] == 1) ans++;
     }
-    cout << freq;
+    cout << ans;
+    /*
+    x가 짝수일 때에는 (x/2 - 1)까지, 홀수일 때에는 (x-1)/2까지 탐색할 수 있도록 하기 위해서
+    i < (x+1)/2로 뒀음. 직접 x가 10일 때 / 11일 때를 생각해보면 이해 가능
+    이 코드는 O(n+x)에 동작, x가 최대 200만이므로 통과에는 문제가 없으나 O(n)에 비해서는 느림.
+    */
 }
